89add189af8499198ce7c856df947ae886f6b9e2
seba.developer <cmoralesaroca@gmail.com>
2023-02-21 15:28:07 -0300

    finished

--- sidebar.js
+++ sidebar.js
@@ -3,16 +3,22 @@ console.log("hello") ;
 const btnBurger = document.getElementById("btnBurger");
 const containerMenu = document.getElementById("containerMenu");
 const menu = document.getElementById("menu");
+const btncancelar = document.getElementById("btncancelar") ;
 
 
+let valor = 0 ;
 btnBurger.addEventListener("click", function container (onclick) {
-    let valor = 0 ;
     valor = valor + 1 ;
-    console.log (valor) ;
+    console.log("menu a la vista.") ;
     if (valor == 1) {
         containerMenu.style.display = "block";
     } else {
         valor = 0 ;
-        containerMenu.style.display = "block" ;
+        containerMenu.style.display = "none" ;
+        console.log("menu oculto") ;
     }
+});
+
+btncancelar.addEventListener("click", () => {
+    containerMenu.style.display = "none" ;
 });
