5826294f56432a7e0675b5a27d17c0606b1b956d
seba.developer <cmoralesaroca@gmail.com>
2023-02-21 00:13:10 -0300

    css listo ahora solo queda codificar el js

---
+++ sidebar.js
@@ -0,0 +1,18 @@
+console.log("hello") ;
+
+
+
+/*
+valor = 0 ;
+
+magico.addEventListener("click", () => {
+    valor = valor + 1 ;
+    console.log (valor) ;
+    if (valor == 1) {
+        boton.style.display = "none";
+    } else {
+        valor = 0 ;
+        boton.style.display = "block"
+    }
+});
+*/
